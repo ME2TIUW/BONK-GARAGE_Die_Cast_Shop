a2ecb5e407048668052224f32502912a6248ed69
ME2TIUW <155435540+ME2TIUW@users.noreply.github.com>
2024-06-12 12:27:21 +0700

    BONK-GARAGE_Die_Cast_Shop

---
+++ app/src/main/java/com/example/bonkgarage/CarDataManager.kt
@@ -0,0 +1,26 @@
+package com.example.bonkgarage
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class CarDataManager(private val db: FirebaseFirestore) {
+
+    fun addCarDataToFirestore(carDataList: List<HashMap<String, Any>>) {
+        val db = FirebaseFirestore.getInstance()
+        val colRef = db.collection("car_models") // Replace with your collection name
+
+        // Check if collection exists before adding data
+        colRef.get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val documents = task.result
+                    if (documents.isEmpty) {  // Collection doesn't exist, add data
+                        for (carData in carDataList) {
+                            colRef.add(carData)
+                        }
+                    } else {
+                        print("Car Data Already Exists")
+                    }
+                }
+            }
+    }
+}
